6aa46176055b801aa05cd6b397b4a8daf5b13ade
Akaki Khuntsaria <paranoid739@gmail.com>
2021-06-20 15:43:55 +0400

    Add configuration of version and rounds

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import bcrypt from 'bcryptjs';
 
-document.getElementById("button-submit").onclick = (event) => {
-    const stringToEncrypt = document.getElementById("string-to-encrypt").value;
+document.getElementById('button-submit').onclick = (event) => {
+    const rounds = parseInt(document.getElementById('input-rounds').value) || 10;
+    const stringToEncrypt = document.getElementById('input-string-to-encrypt').value;
+    const version = document.getElementById('input-version').value;
 
-    bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.genSalt(rounds, (err, salt) => {
         bcrypt.hash(stringToEncrypt, salt, (err, hash) => {
-            document.getElementById("result").innerHTML = hash;
+            hash = hash.replace('$2a$', version);
+
+            document.getElementById('result').innerHTML = hash;
         });
     });
-
-    return false;
 }
